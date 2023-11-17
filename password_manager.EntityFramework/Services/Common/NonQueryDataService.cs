29c4e35c8a52e86930134960c39c854e2a896c41
Szpachlarz <kl.kolakowska@gmail.com>
2023-11-17 16:42:42 +0100

    services

---
+++ password_manager.EntityFramework/Services/Common/NonQueryDataService.cs
@@ -0,0 +1,54 @@
+﻿using Microsoft.EntityFrameworkCore.ChangeTracking;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace password_manager.EntityFramework.Services.Common
+{
+    public class NonQueryDataService<T> where T : class
+    {
+        private readonly password_managerDbContextFactory _contextFactory;
+        public NonQueryDataService(password_managerDbContextFactory contextFactory)
+        {
+            _contextFactory = contextFactory;
+        }
+
+        public async Task<T> Create(T entity)
+        {
+            using (password_managerDbContext context = _contextFactory.Create())
+            {
+                EntityEntry<T> createdResult = await context.Set<T>().AddAsync(entity);
+                await context.SaveChangesAsync();
+
+                return createdResult.Entity;
+            }
+        }
+
+        public async Task<T> Update(int id, T entity)
+        {
+            using (password_managerDbContext context = _contextFactory.Create())
+            {
+                entity.Id = id;
+
+                context.Set<T>().Update(entity);
+                await context.SaveChangesAsync();
+
+                return entity;
+            }
+        }
+
+        public async Task<bool> Delete(int id)
+        {
+            using (password_managerDbContext context = _contextFactory.Create())
+            {
+                T entity = await context.Set<T>().FirstOrDefaultAsync((e) => e.Id == id);
+                context.Set<T>().Remove(entity);
+                await context.SaveChangesAsync();
+
+                return true;
+            }
+        }
+    }
+}
---
+++ password_manager.EntityFramework/Services/GenericDataService.cs
@@ -0,0 +1,42 @@
+﻿using password_manager.Domain.Services;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace password_manager.EntityFramework.Services
+{
+    public class GenericDataService<T> : IDataService<T> where T : class
+    {
+        public GenericDataService()
+        {
+
+        }
+
+        public Task<T> Create(T entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<bool> Delete(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<T> Get(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<IEnumerable<T>> GetAll()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<T> Update(int id, T entity)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
