f4d7946e48d5d65383cafb4ca8df00998d794d6b
Dominik Piskor <dominikpiskor1@wp.pl>
2023-12-11 05:31:52 +0100

    Added detection of url

--- PasswordManager.WPF/Components/UserPanelViewTable.xaml.cs
+++ PasswordManager.WPF/Components/UserPanelViewTable.xaml.cs
@@ -12,6 +12,8 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using PasswordManager.WPF.Models;
+using PasswordManager.WPF.ViewModels;
 
 namespace PasswordManager.WPF.Components
 {
@@ -20,9 +22,45 @@ namespace PasswordManager.WPF.Components
     /// </summary>
     public partial class UserPanelViewTable : UserControl
     {
+        private UserPanelViewModel ViewModel => (UserPanelViewModel)DataContext;
         public UserPanelViewTable()
         {
             InitializeComponent();
+        }
+        
+        private void TextBlock_MouseEnter(object sender, MouseEventArgs e)
+        {
+            if (sender is not FrameworkElement element) return;
+            if (element.DataContext is not UserRecord record) return;
+            if(record.HasUrl)
+                Mouse.OverrideCursor = Cursors.Hand;
+        }
+
+        private void TextBlock_MouseLeave(object sender, MouseEventArgs e)
+        {
+            Mouse.OverrideCursor = null;
+        }
+        
+        private void Button_MouseEnter(object sender, MouseEventArgs e)
+        {
+            Mouse.OverrideCursor = Cursors.Hand;
+        }
+
+        private void Button_MouseLeave(object sender, MouseEventArgs e)
+        {
+            Mouse.OverrideCursor = null; 
+        }
+
+        private void TextBlock_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+        {
+            if (DataContext is UserPanelViewModel viewModel)
+            {
+                if(sender is not FrameworkElement element) return;
+                if(element.DataContext is not UserRecord record) return;
+                if(!record.HasUrl) return;
+                if (String.IsNullOrEmpty(record.Website)) return;
+                viewModel.OnLinkClickCommand.Execute(record.Website);
+            }
         }
     }
 }
