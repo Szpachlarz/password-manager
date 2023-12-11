45f1ff077fc94fda2f126b3b0104582fc48ef9a4
Dominik Piskor <dominikpiskor1@wp.pl>
2023-12-11 05:23:14 +0100

    Added possibility of editing user accounts

---
+++ PasswordManager.WPF/Commands/EditRecordCommand.cs
@@ -0,0 +1,51 @@
+using Microsoft.EntityFrameworkCore.Migrations.Operations;
+using PasswordManager.Domain.Models;
+using PasswordManager.Domain.Services.RecordServices;
+using PasswordManager.WPF.State.Accounts;
+using PasswordManager.WPF.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Controls;
+using HandyControl.Controls;
+using NETCore.Encrypt;
+using PasswordManager.WPF.Models;
+using PasswordManager.WPF.State.Navigators;
+
+namespace PasswordManager.WPF.Commands
+{
+    public class EditRecordCommand : AsyncCommandBase
+    {
+        private readonly IRecordService _recordService;
+        private readonly IAccountStore _accountStore;
+        private readonly IRenavigator _renavigator;
+        private readonly EditRecordViewModel _editRecordViewModel;
+
+        public EditRecordCommand(IRecordService recordService, IAccountStore accountStore, IRenavigator renavigator, EditRecordViewModel editRecordViewModel)
+        {
+            _recordService = recordService;
+            _accountStore = accountStore;
+            _renavigator = renavigator;
+            _editRecordViewModel = editRecordViewModel;
+        }
+
+        public override async Task ExecuteAsync(object parameter)
+        {
+            var aes = await _recordService.GetAES(_accountStore.CurrentUser);
+            Record record = new();
+            record.Id = _editRecordViewModel.Id;
+            record.Title = _editRecordViewModel.Title;
+            record.Website = _editRecordViewModel.Website;
+            record.Username = _editRecordViewModel.Username;
+            record.Password = EncryptProvider.AESEncrypt(_editRecordViewModel.Password, aes.Item1, aes.Item2);
+            record.Description = _editRecordViewModel.Description;
+            await _recordService.UpdateRecord(record.Id, _accountStore.CurrentUser, record.Title, record.Website,
+                record.Username, record.Password, record.Description);
+            Growl.Success("Pomyślnie edytowano rekord!");
+            _renavigator.Renavigate();
+        }
+    }
+}
---
+++ PasswordManager.WPF/ViewModels/EditRecordViewModel.cs
@@ -0,0 +1,168 @@
+﻿using PasswordManager.Domain.Services.RecordServices;
+using PasswordManager.WPF.Commands;
+using PasswordManager.WPF.State.Accounts;
+using PasswordManager.WPF.State.Authenticators;
+using PasswordManager.WPF.State.Navigators;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+using NETCore.Encrypt;
+using PasswordManager.Domain.Models;
+
+namespace PasswordManager.WPF.ViewModels
+{
+    public class EditRecordViewModel : ViewModelBase
+    {
+        private string _title;
+        public string Title
+        {
+            get
+            {
+                return _title;
+            }
+            set
+            {
+                _title = value;
+                OnPropertyChanged(nameof(Title));
+                OnPropertyChanged(nameof(CanSubmit));
+            }
+        }
+
+        private int _id;
+        public int Id
+        {
+            get
+            {
+                return _id;
+            }
+            set
+            {
+                _id = value;
+                OnPropertyChanged(nameof(Id));
+            }
+        }
+        
+        private string _website;
+        public string Website
+        {
+            get
+            {
+                return _website;
+            }
+            set
+            {
+                _website = value;
+                OnPropertyChanged(nameof(Website));
+                OnPropertyChanged(nameof(CanSubmit));
+            }
+        }
+
+        private string _username;
+        public string Username
+        {
+            get
+            {
+                return _username;
+            }
+            set
+            {
+                _username = value;
+                OnPropertyChanged(nameof(Username));
+                OnPropertyChanged(nameof(CanSubmit));
+            }
+        }
+
+        private string _password;
+        public string Password
+        {
+            get
+            {
+                return _password;
+            }
+            set
+            {
+                _password = value;
+                OnPropertyChanged(nameof(Password));
+                OnPropertyChanged(nameof(CanSubmit));
+            }
+        }
+
+        private string _description;
+        public string Description
+        {
+            get
+            {
+                return _description;
+            }
+            set
+            {
+                _description = value;
+                OnPropertyChanged(nameof(Description));
+            }
+        }
+
+        private bool _isSubmitting;
+        public bool IsSubmitting
+        {
+            get
+            {
+                return _isSubmitting;
+            }
+            set
+            {
+                _isSubmitting = value;
+                OnPropertyChanged(nameof(IsSubmitting));
+            }
+        }
+
+        private string _errorMessage;
+        public string ErrorMessage
+        {
+            get
+            {
+                return _errorMessage;
+            }
+            set
+            {
+                _errorMessage = value;
+                OnPropertyChanged(nameof(ErrorMessage));
+                OnPropertyChanged(nameof(HasErrorMessage));
+            }
+        }
+
+        public bool HasErrorMessage => !string.IsNullOrEmpty(ErrorMessage);
+        public bool CanSubmit => !string.IsNullOrEmpty(Title) && !string.IsNullOrEmpty(Website) && !string.IsNullOrEmpty(Username) && !string.IsNullOrEmpty(Password) /*&& Title.Length > 5 && Title.Length < 50 && Website.Length < 300 && Username.Length > 4 && Username.Length < 30 && Password.Length > 7 && Password.Length < 32*/;
+
+        public ICommand SubmitCommand { get; set; }
+        public ICommand ViewUserPanelCommand { get; set; }
+
+        public EditRecordViewModel(IAccountStore accountStore, IRecordService recordService, IRenavigator userPanelRenavigator)
+        {
+            Initialize(accountStore, recordService, userPanelRenavigator);
+        }
+        
+        private async void Initialize(IAccountStore accountStore, IRecordService recordService, IRenavigator userPanelRenavigator)
+        {
+            _id = accountStore.SelectedRecord.Id;
+            _password = await FillPassword(recordService, accountStore);
+            _username = accountStore.SelectedRecord.Username;
+            _website = accountStore.SelectedRecord.Website;
+            _title = accountStore.SelectedRecord.Title;
+            _description = accountStore.SelectedRecord.Description;
+            ViewUserPanelCommand = new RenavigateCommand(userPanelRenavigator);
+            SubmitCommand = new EditRecordCommand(recordService, accountStore, userPanelRenavigator, this);
+        }
+
+        private async Task<string> FillPassword(IRecordService recordService, IAccountStore accountStore)
+        {
+            var aes = await recordService.GetAES(accountStore.CurrentUser);
+            var password = await recordService.GetPasswordById(accountStore.SelectedRecord.Id);
+            var passwordResult = EncryptProvider.AESDecrypt(password, aes.Item1, aes.Item2);
+            return passwordResult;
+        }
+    }
+}
