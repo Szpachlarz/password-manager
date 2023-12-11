1bf5831079b16e69b4e5de8a2df2b9b911992d2d
Dominik Piskor <dominikpiskor1@wp.pl>
2023-12-11 05:32:39 +0100

    Improved view

--- PasswordManager.WPF/ViewModels/UserPanelViewModel.cs
+++ PasswordManager.WPF/ViewModels/UserPanelViewModel.cs
@@ -10,12 +10,31 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Input;
+using System.Windows.Media;
+using HandyControl.Tools;
+using PasswordManager.Domain.Models;
+using PasswordManager.WPF.Models;
 
 namespace PasswordManager.WPF.ViewModels
 {
     public class UserPanelViewModel : ViewModelBase
     {
         private string _errorMessage;
+        private List<UserRecord> _recordsList;
+        private Brush _backgroundColor = Brushes.Aqua;
+        public Brush BackgroundColor
+        {
+            get
+            {
+                return _backgroundColor;
+            }
+            set
+            {
+                _backgroundColor = value;
+                OnPropertyChanged(nameof(BackgroundColor));
+            }
+        }
+        
         public string ErrorMessage
         {
             get
@@ -30,16 +49,51 @@ namespace PasswordManager.WPF.ViewModels
             }
         }
 
+        private string _username;
+
+        public string Username
+        {
+            get { return _username; }
+            set
+            {
+                _username = value;
+                OnPropertyChanged(nameof(Username));
+            }
+        }
+
+        public List<UserRecord> RecordsList
+        {
+            get
+            {
+                return _recordsList;
+            }
+            set
+            {
+                _recordsList = value;
+                OnPropertyChanged(nameof(RecordsList));
+            }
+        }
+
         public bool HasErrorMessage => !string.IsNullOrEmpty(ErrorMessage);
 
         public ICommand LoadRecordsCommand { get; }
         public ICommand ViewAddRecordCommand { get; }
+        public ICommand DeleteRecordCommand { get; }
+        public ICommand MoveToEditRecordCommand { get; }
+        public ICommand OnLinkClickCommand { get; }
+        public ICommand CopyPasswordToClipboardCommand { get; }
         public ICommand UserLogout { get; }
-        public UserPanelViewModel(IRenavigator addRecordRenavigator, IRecordService recordService, IAccountStore accountStore, IAuthenticator authenticator, IRenavigator loginRenavigator)
+        public UserPanelViewModel(IRenavigator addRecordRenavigator, IRenavigator editRecordRenavigator, IRecordService recordService, IAccountStore accountStore, IAuthenticator authenticator, IRenavigator loginRenavigator)
         {
+            Username = accountStore.CurrentUser.AccountHolder.Username;
             ViewAddRecordCommand = new RenavigateCommand(addRecordRenavigator);
-            LoadRecordsCommand = new LoadRecordsCommand(recordService, accountStore);
+            LoadRecordsCommand = new LoadRecordsCommand(recordService, accountStore, this);
+            MoveToEditRecordCommand = new MoveToEditRecordCommand(accountStore, editRecordRenavigator, this);
+            DeleteRecordCommand = new DeleteRecordCommand(recordService, accountStore, this);
+            OnLinkClickCommand = new OnLinkClickCommand();
             UserLogout = new LogoutCommand(this, authenticator, loginRenavigator);
+            CopyPasswordToClipboardCommand = new CopyPasswordToClipboardCommand(recordService, accountStore, this);
+            LoadRecordsCommand.Execute(null);
         }
 
     }
